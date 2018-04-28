f084121a68655d4beadbba080fa80323e9f97b91
yaoyuanliang <yaoyuanliang@aikucun.com>
2018-04-28 16:19:25 +0800

    update code

--- src/main/java/com/demo3/ProcessDefinitionTest.java
+++ src/main/java/com/demo3/ProcessDefinitionTest.java
@@ -12,7 +12,10 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipInputStream;
 
 /**
@@ -151,4 +154,60 @@ public class ProcessDefinitionTest {
         //将输入流的图片写到D盘下
         FileUtils.copyInputStreamToFile(in, file);
     }
+
+    /***附加功能：查询最新版本的流程定义*/
+    @Test
+    public void findLastVersionProcessDefinition(){
+        List<ProcessDefinition> list = processEngine.getRepositoryService()//
+                .createProcessDefinitionQuery()//
+                .orderByProcessDefinitionVersion().asc()//使用流程定义的版本升序排列
+                .list();
+        /**
+         * Map<String,ProcessDefinition>
+         map集合的key：流程定义的key
+         map集合的value：流程定义的对象
+         map集合的特点：当map集合key值相同的情况下，后一次的值将替换前一次的值
+         */
+        Map<String, ProcessDefinition> map = new LinkedHashMap<String, ProcessDefinition>();
+        if(list!=null && list.size()>0){
+            for(ProcessDefinition pd:list){
+                map.put(pd.getKey(), pd);
+            }
+        }
+        List<ProcessDefinition> pdList = new ArrayList<ProcessDefinition>(map.values());
+        if(pdList!=null && pdList.size()>0){
+            for(ProcessDefinition pd:pdList){
+                System.out.println("流程定义ID:"+pd.getId());//流程定义的key+版本+随机生成数
+                System.out.println("流程定义的名称:"+pd.getName());//对应helloworld.bpmn文件中的name属性值
+                System.out.println("流程定义的key:"+pd.getKey());//对应helloworld.bpmn文件中的id属性值
+                System.out.println("流程定义的版本:"+pd.getVersion());//当流程定义的key值相同的相同下，版本升级，默认1
+                System.out.println("资源名称bpmn文件:"+pd.getResourceName());
+                System.out.println("资源名称png文件:"+pd.getDiagramResourceName());
+                System.out.println("部署对象ID："+pd.getDeploymentId());
+                System.out.println("#########################################################");
+            }
+        }
+    }
+
+    /**附加功能：删除流程定义（删除key相同的所有不同版本的流程定义）*/
+    @Test
+    public void deleteProcessDefinitionByKey(){
+        //流程定义的key
+        String processDefinitionKey = "helloworld";
+        //先使用流程定义的key查询流程定义，查询出所有的版本
+        List<ProcessDefinition> list = processEngine.getRepositoryService()//
+                .createProcessDefinitionQuery()//
+                .processDefinitionKey(processDefinitionKey)//使用流程定义的key查询
+                .list();
+        //遍历，获取每个流程定义的部署ID
+        if(list!=null && list.size()>0){
+            for(ProcessDefinition pd:list){
+                //获取部署ID
+                String deploymentId = pd.getDeploymentId();
+                processEngine.getRepositoryService()//
+                        .deleteDeployment(deploymentId, true);
+            }
+        }
+    }
+
 }
